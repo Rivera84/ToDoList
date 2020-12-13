740bf019ca2312eee895a1cc031c59527035fbd5
Luis Rivera <luisrivera084@gmail.com>
2020-12-12 18:15:16 -0600

    diseño de pantalla de inicio de sesion y crear usuario

--- frontend-ToDoList/src/app/view/iniciar_sesion/iniciar_sesion.component.ts
+++ frontend-ToDoList/src/app/view/iniciar_sesion/iniciar_sesion.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { NgForm, FormControlDirective } from '@angular/forms';
 import swal from 'sweetalert2';
 import { AppService } from '../../app.service';
+import * as jQuery from 'jquery';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -20,6 +21,27 @@ export class IniciarSesionComponent implements OnDestroy {
   }
 
   constructor(public service: AppService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    $(function() {
+
+        $('#login-form-link').click(function(e) {
+            $(".login-form").delay(100).fadeIn(100);
+             $("#register-form").fadeOut(100);
+            $('#register-form-link').removeClass('active');
+            $(this).addClass('active');
+            e.preventDefault();
+        });
+        $('#register-form-link').click(function(e) {
+            $("#register-form").delay(100).fadeIn(100);
+             $(".login-form").fadeOut(100);
+            $('#login-form-link').removeClass('active');
+            $(this).addClass('active');
+            e.preventDefault();
+        });
+    
+    });
   }
 
   ngOnDestroy() {
